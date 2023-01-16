767de2f097d438e409f0cedc37ac3438197039af
DNML31 <danielchun31@gmail.com>
2023-01-16 15:39:31 -0800

    Create class Word, initial game set up
    This commit adds a Word class with class methods set_up and
    make_blank_spaces. The initialization takes two parameters, the word
    from the txt file and how many characters that word has.
    
    Once the Word object is created, it becomes the argument for the
    play_game method which displays a greeting message and the number of
    blanks for the user to see.
    
    Currently, the blanks are printed before the message even though the
    order is the other way around in the method. Another issue is that the
    user's input is always a String type even if a number is typed in. This
    means the if..elsif block isn't telling the user "not a letter", and
    "Choose a letter".

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,58 @@
-new_word = File.open('google-10000-english-no-swears.txt')
+class Word
+  def initialize(word, spaces)
+    @word = word
+    @spaces = spaces
+  end
 
-new_word = new_word.readlines(chomp: true)
-word = new_word.sample
+  def make_blank_spaces
+    @spaces.times do
+      print "_ "
+    end
+  end
+
+  def set_up
+    make_blank_spaces
+  end
+end
+
+open_txt = File.open('google-10000-english-no-swears.txt')
+open_txt = open_txt.readlines(chomp: true) # array
+
+word = open_txt.sample
 
 until word.length >= 5 && word.length <= 12 do
-  word = new_word.sample
+  word = open_txt.sample
 end
-p word
 
-word_length = word.length
-p word_length
+word_spaces = word.length
+guess_this_word = Word.new(word, word_spaces)
+
+
+def play_game(secret_word)
+  # want it to say this message and show blank spaces underneath
+  print "\nLet's play hangman! Guess a letter:  "
+
+  secret_word 
+  # creates the blanks above the greeting message
+
+  guess = gets.chomp
+
+  if guess.length > 1
+    until guess.length == 1 do
+      puts "Choose only one letter."
+      guess = gets.chomp
+    end
+    # insert check method (needs to re-display current state of the game
+  elsif guess.class != String
+    puts "not a letter"
+    until guess.class == String do
+      puts "Choose a letter."
+    end
+  else
+    puts "checking..."
+    # insert check method (needs to re-display current state of the game
+  end
+  
+end
+
+play_game(guess_this_word.set_up)
