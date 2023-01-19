ad14c8b93ef8e4fc252357f65c4ff6e0bfcfd396
DNML31 <danielchun31@gmail.com>
2023-01-18 18:41:42 -0800

    Separate class methods and find_index method
    This commit breaks apart the Word class method, set_up to be two
    methods, blank_spaces and word. This was done to make it easier to
    understand what happens when a new Word object is made. These two
    class methods will also be two arguments for the play_game method.
    
    The find_index method finds where the user's guess is within the word
    array and returns the index number. This however, does not solve the
    issue of whether there are multiple instances of the same letter
    elsewhere in the word.
    
    There have also been some variable name changes to make more sense.

--- hangman.rb
+++ hangman.rb
@@ -4,48 +4,57 @@ class Word
     @spaces = spaces
   end
 
-  def make_blank_spaces
+  # create blank spaces and an array of blank spaces
+  def blank_spaces
+    spaces = []
     @spaces.times do
+      spaces.push("_ ")
       print "_ "
     end
   end
 
-  def set_up
-    make_blank_spaces
+  # return the word
+  def word
+    @word
   end
+
 end
 
 open_txt = File.open('google-10000-english-no-swears.txt')
 open_txt = open_txt.readlines(chomp: true) # array
-
 word = open_txt.sample
-
 until word.length >= 5 && word.length <= 12 do
   word = open_txt.sample
 end
 
-word_spaces = word.length
-guess_this_word = Word.new(word, word_spaces)
+spaces = word.length
+solve_me = Word.new(word, spaces)
 
-def play_game(secret_word)
+def find_index(guess, word)
+  print word.to_a.index {|element| element == guess}
+  # get the WORD and turn it into an array
+  # find the index where the GUESS is in the WORD array
+  # return the index number
 
+end
+
+def play_game(word, word_spaces)
+  p word_spaces
+  p word
   letters = Range.new('a','z').to_a
   numbers = Range.new('0','9').to_a
-  # want it to say this message and show blank spaces underneath
+  word_array = word.split(//)
+
   print "\nLet's play hangman! Guess a letter:  "
-
-  secret_word
-  # method creates the blanks above the greeting message
-
   guess = gets.chomp
 
+  # user guess filtering for valid input
   if numbers.include?(guess)
     puts "not a letter"
     until letters.include?(guess) do
       puts "Choose a letter."
       guess = gets.chomp
     end
-
   elsif guess.length > 1
     until guess.length == 1 do
       puts "Choose only one letter."
@@ -56,11 +65,23 @@ def play_game(secret_word)
         guess = gets.chomp
       end
     end
-
   else
     puts "checking..."
     # insert check method (needs to re-display current state of the game
   end
+
+  if word_array.include?(guess)
+    puts 'correct'
+
+    find_index(guess, word_array)
+    # this method returns the index of the word_array where the guess is
+  else
+    puts 'incorrect'
+    # if incorrect letter, display a message
+    # -= number of guesses left
+    # repeat guess sequence
+  end
+
 end
 
-play_game(guess_this_word.set_up)
+play_game(solve_me.word, solve_me.blank_spaces)
