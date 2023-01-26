e9531d693de9bd06d690e77d2f2fda28f84eeec4
DNML31 <danielchun31@gmail.com>
2023-01-25 18:23:17 -0800

    Add correct_letters array and lose message
    This commit prompts the user when they've made a guess that they've
    already tried, whether it's correct or not. An array, correct_letters,
    is initialized within play_game before the loop to take in letters that
    are in the word_array.
    
    Near the beginning of the loop inside play_game, a while loop checks
    whether the user has guessed a correct or incorrect guess and prompt
    for another guess if necessary.
    
    If the user runs out of guess (tries == 0), the word is revealed along
    with a 'you lose' message.

--- hangman.rb
+++ hangman.rb
@@ -13,7 +13,7 @@ class Word
 end
 
 print "************************"
-puts "\nLet's play hangman! You will have 10 tries to figure out the mystery word.
+puts "\nLet's play hangman! You will have 10 tries to figure out the word.
 \nInput only ONE alphabetical letter per turn (no numbers or symbols).\n\n"
 
 open_txt = File.open('google-10000-english-no-swears.txt')
@@ -28,10 +28,6 @@ solve_me = Word.new(word, spaces)
 
 def check_guess(spaces_array, word_array, guess)
 
-  # wrong_letters = []
-  # wrong_letters keeps getting reassigned at the start of this method
-  # everytime it is called, it is emptied
-
   if word_array.include?(guess)
     print "\n**********\n"
     puts "\nCORRECT.\n"
@@ -43,12 +39,9 @@ def check_guess(spaces_array, word_array, guess)
     end
 
   else
-    # wrong_letters.push(guess)
     print "\n**********\n"
     puts "\nINCORRECT. TRY AGAIN.\n"
   end
-
-  # puts "incorrect letters - #{wrong_letters.join(' ')}\n\n"
 
   result = spaces_array.join(' ')
   print result
@@ -60,19 +53,24 @@ def play_game(word, word_spaces, spaces)
   numbers = Range.new('0','9').to_a
   word_array = word.split(//)
 
-  p word_array
+  # p word_array
 
   spaces_array = []
   spaces.times do spaces_array.push("-") end
 
   tries = 10
 
+  correct_letters = []
   wrong_letters = []
 
   loop do
     print "\nTries left: #{tries}. Guess a letter:  "
-    # need to remember ALL already guessed letters
     guess = gets.chomp
+
+    while correct_letters.include?(guess) || wrong_letters.include?(guess)
+      puts "\nYou've already guessed this letter. Try another: "
+      guess = gets.chomp
+    end
 
     if numbers.include?(guess)
       puts "Not a letter..."
@@ -92,24 +90,24 @@ def play_game(word, word_spaces, spaces)
       end
 
     else
-      # because current keeps getting reassigned within the loop, the 
-      # wrong_letters array keeps resetting?
       current = check_guess(spaces_array, word_array, guess)
       print current.to_s
-      # current is a Nilclass - all referneces to this var are not working
-      # print word_array.join('')
+      # current is a Nilclass - all referneces to this var are nil
     end
 
     if word.include?(guess)
       puts ''
+      correct_letters.push(guess)
     elsif word.split.none?(guess)
       tries -= 1
       wrong_letters.push(guess)
     end
 
     puts "\n\nincorrect letters - #{wrong_letters.uniq.join(' ')}"
+
     # loop condition
     if tries == 0
+      puts "You lose. The word was #{word_array.join('').upcase}."
       break
     elsif word_array.to_s == current.to_s
       break
